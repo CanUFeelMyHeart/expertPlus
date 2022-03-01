cd08dc607dfbf1791e51f9c68eaa1ba286eff740
julia snegova <ssnnggvv@gmail.com>
2022-03-01 20:04:45 +0300

    header component is created

--- medical-clinic-expert/src/App.js
+++ medical-clinic-expert/src/App.js
@@ -1,5 +1,10 @@
+import React from "react";
+import { HeaderComponent } from "../src/components/HeaderComponent/HeaderComponent";
+
 function App() {
-  return <div />
+    return <>
+        <HeaderComponent />
+    </>;
 }
 
 export default App;
