48358b7812148879a6fa76e2e90fab04e2de51ab
julia snegova <ssnnggvv@gmail.com>
2022-03-03 17:47:49 +0300

    aboutClinicBlock image blick is set

--- medical-clinic-expert/src/components/AboutClinicComponent/AboutClinicBlockComponent/AboutClinicBlockComponent.jsx
+++ medical-clinic-expert/src/components/AboutClinicComponent/AboutClinicBlockComponent/AboutClinicBlockComponent.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import './AboutClinicBlockComponent.css'
+import "./AboutClinicBlockComponent.css";
+import testImage from '../../../img/logo.svg'
 
 export const AboutClinicBlockComponent = () => {
     return (
         <div className="aboutClinicBlock">
-rgthrthrthrthrth
+            <div className="aboutClinicBlock__img"><img src={testImage}></img></div>
+            <div className="aboutClinicBlock__header"></div>
+            <div className="aboutClinicBlock__description"></div>
         </div>
     );
 };
