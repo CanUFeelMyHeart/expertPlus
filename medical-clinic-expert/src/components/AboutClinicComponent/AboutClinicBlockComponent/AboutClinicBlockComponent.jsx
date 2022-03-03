72f1cdfb1e97e50e06f499b73c45a55e63075ef6
julia snegova <ssnnggvv@gmail.com>
2022-03-03 17:54:56 +0300

    styles for components in aboutClinicBock is set

--- medical-clinic-expert/src/components/AboutClinicComponent/AboutClinicBlockComponent/AboutClinicBlockComponent.jsx
+++ medical-clinic-expert/src/components/AboutClinicComponent/AboutClinicBlockComponent/AboutClinicBlockComponent.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import "./AboutClinicBlockComponent.css";
-import testImage from '../../../img/logo.svg'
+import testImage from "../../../img/logo.svg";
 
 export const AboutClinicBlockComponent = () => {
     return (
         <div className="aboutClinicBlock">
-            <div className="aboutClinicBlock__img"><img src={testImage}></img></div>
-            <div className="aboutClinicBlock__header"></div>
-            <div className="aboutClinicBlock__description"></div>
+            <div className="aboutClinicBlock__img">
+                <img src={testImage}></img>
+            </div>
+            <div className="aboutClinicBlock__header">
+                <h2>Спец узкого профиля</h2>
+            </div>
+            <div className="aboutClinicBlock__description">
+                <h3>
+                    Онколог-маммолог Аллерголог-иммунолог, пульмонолог •
+                    Колопроктолог • Дерматолог • Сердечно-сосудистый хирург
+                </h3>
+            </div>
         </div>
     );
 };
