ea609908d169d43a0f7a4af7b56d19a091790bda
Mikhail <msmerkushov123@yandex.ru>
2022-03-03 14:19:26 +0300

    added bg-image in infoComponent

--- medical-clinic-expert/src/components/InformationComponent/InformationComponent.jsx
+++ medical-clinic-expert/src/components/InformationComponent/InformationComponent.jsx
@@ -6,10 +6,11 @@ export const InformationComponent = () => {
     return (
         
       
-        <div className="<informationBlock>">
-               
-            <h3 className="informationHeading">Частная медицинская клиника «Эксперт Плюс»</h3> 
+        <div className="informationBlock">
             
+            <div className="informationTextContainer"> 
+                <h3 className="informationHeading">Частная медицинская клиника «Эксперт Плюс»</h3>
+            </div>
         </div>
       
     );
