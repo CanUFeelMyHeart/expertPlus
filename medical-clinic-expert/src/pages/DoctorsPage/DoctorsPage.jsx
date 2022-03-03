69a43a07d99de573a60d43ae0ccf108fe3783c8d
julia snegova <ssnnggvv@gmail.com>
2022-03-03 22:46:01 +0300

    adding components to pages is done

--- medical-clinic-expert/src/pages/DoctorsPage/DoctorsPage.jsx
+++ medical-clinic-expert/src/pages/DoctorsPage/DoctorsPage.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { AboutClinicComponent as AboutClinic } from "../../components/AboutClinicComponent/AboutClinicComponent";
+import { ServicesComponent } from "../../components/ServicesComponent/ServicesComponent";
 
 export const DoctorsPage = () => {
     return (
         <>
             <div>doctors page</div>
+            <AboutClinic />
+            <ServicesComponent />
         </>
     );
 };
--- medical-clinic-expert/src/pages/MainPage/MainPage.jsx
+++ medical-clinic-expert/src/pages/MainPage/MainPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { AboutClinicComponent as AboutClinic } from "../../components/AboutClinicComponent/AboutClinicComponent";
 import { ServicesComponent } from '../../components/ServicesComponent/ServicesComponent'
+
 export const MainPage = () => {
     return (
         <>
