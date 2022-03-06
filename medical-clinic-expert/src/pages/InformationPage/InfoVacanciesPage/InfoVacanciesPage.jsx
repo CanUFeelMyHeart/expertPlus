31f87235dae339eeaaee414f86f845023155d6b3
julia snegova <ssnnggvv@gmail.com>
2022-03-06 05:31:00 +0300

    vacancies page is set

--- medical-clinic-expert/src/pages/InformationPage/InfoVacanciesPage/InfoVacanciesPage.jsx
+++ medical-clinic-expert/src/pages/InformationPage/InfoVacanciesPage/InfoVacanciesPage.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import { AboutClinicComponent } from "../../../components/AboutClinicComponent/AboutClinicComponent";
 import { ServicesComponent } from "../../../components/ServicesComponent/ServicesComponent";
+import { InformationContainerComponent } from "../../../components/InformationContainerComponent/InformationContainerComponent";
+import "./InfoVacanciesPage.css";
 
 export const InfoVacanciesPage = () => {
     return (
         <div className="InfoVacanciesPage">
-            <h2>Вакансии block</h2>
+            <InformationContainerComponent header="Вакансии">
+                <h3 className="InfoVacanciesPage__header">
+                    На данный момент в клинике открыты вакансии:
+                </h3>
+                <br />
+                <ul className="InfoVacanciesPage_list">
+                    <li>Врач эндокринолог</li>
+                    <li>Врач ревматолог</li>
+                    <li>Врач аллерголог пульмонолог</li>
+                </ul>
+            </InformationContainerComponent>
             <AboutClinicComponent />
             <ServicesComponent />
         </div>
