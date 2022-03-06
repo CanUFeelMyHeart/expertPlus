4cce81758be107f5fc52f647983aef189a7cc828
julia snegova <ssnnggvv@gmail.com>
2022-03-06 21:51:51 +0300

    qualifications info added

--- medical-clinic-expert/src/pages/InformationPage/InfoQualificationsPage/InfoQualificationsPage.jsx
+++ medical-clinic-expert/src/pages/InformationPage/InfoQualificationsPage/InfoQualificationsPage.jsx
@@ -2,11 +2,47 @@ import React from "react";
 import { AboutClinicComponent } from "../../../components/AboutClinicComponent/AboutClinicComponent";
 import { ServicesComponent } from "../../../components/ServicesComponent/ServicesComponent";
 import { InformationContainerComponent } from "../../../components/InformationContainerComponent/InformationContainerComponent";
+import { qualificationsInfo } from "./QualificationsInfo";
+import "./InfoQualificationsPage.css";
 
 export const InfoQualificationsPage = () => {
     return (
         <div className="InfoQualificationsPage">
-            <InformationContainerComponent header="Сведения о квалификации медицинских работников, участвующих в предоставлении платных медицинских услуг"></InformationContainerComponent>
+            <InformationContainerComponent header="Сведения о квалификации медицинских работников, участвующих в предоставлении платных медицинских услуг">
+                <table>
+                    <thead>
+                        <tr className="InfoQualificationsPage__thead">
+                            <td>Ф.И.О. врача</td>
+                            <td>Специальность</td>
+                            <td>Год выдачи, город</td>
+                            <td>Срок действия сертификата</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {qualificationsInfo.map((info, index) => (
+                            <>
+                                <tr
+                                    key={index}
+                                    className="InfoQualificationsPage__trow"
+                                >
+                                    <td className="InfoQualificationsPage__trow_td name">
+                                        {info.name}
+                                    </td>
+                                    <td className="InfoQualificationsPage__trow_td">
+                                        {info.speciality}
+                                    </td>
+                                    <td className="InfoQualificationsPage__trow_td">
+                                        {info.yearSity}
+                                    </td>
+                                    <td className="InfoQualificationsPage__trow_td">
+                                        {info.time}
+                                    </td>
+                                </tr>
+                            </>
+                        ))}
+                    </tbody>
+                </table>
+            </InformationContainerComponent>
             <AboutClinicComponent />
             <ServicesComponent />
         </div>
