25cbcad28fcceca943f5a4749234dd8f8102da02
julia snegova <ssnnggvv@gmail.com>
2022-03-06 04:12:04 +0300

    InformationContainerComponent is created

--- medical-clinic-expert/src/pages/DmsPage/DmsPage.jsx
+++ medical-clinic-expert/src/pages/DmsPage/DmsPage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { AboutClinicComponent as AboutClinic } from "../../components/AboutClinicComponent/AboutClinicComponent";
 import { ServicesComponent } from "../../components/ServicesComponent/ServicesComponent";
+import { InformationContainerComponent } from "../../components/InformationContainerComponent/InformationContainerComponent";
 
 export const DmsPage = () => {
     return (
         <>
-            <div>dms page</div>
+            <InformationContainerComponent header="Добровольное медицинское страхование">
+                
+            </InformationContainerComponent>
             <AboutClinic />
             <ServicesComponent />
         </>
