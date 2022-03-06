29590d723a9000126511d94ac454c89e2f6daf8e
julia snegova <ssnnggvv@gmail.com>
2022-03-06 06:39:55 +0300

    InfoContainerComponent is added to official documents page

--- medical-clinic-expert/src/pages/InformationPage/InfoOffDocumentsPage/InfoOffDocumentsPage.jsx
+++ medical-clinic-expert/src/pages/InformationPage/InfoOffDocumentsPage/InfoOffDocumentsPage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { AboutClinicComponent } from "../../../components/AboutClinicComponent/AboutClinicComponent";
 import { ServicesComponent } from "../../../components/ServicesComponent/ServicesComponent";
+import { InformationContainerComponent } from "../../../components/InformationContainerComponent/InformationContainerComponent";
 
 export const InfoOffDocumentsPage = () => {
     return (
         <div className="InfoOffDocumentsPage">
-            <h2>Официальные документы block</h2>
+            <InformationContainerComponent header="Официальные документы"></InformationContainerComponent>
             <AboutClinicComponent />
             <ServicesComponent />
         </div>
