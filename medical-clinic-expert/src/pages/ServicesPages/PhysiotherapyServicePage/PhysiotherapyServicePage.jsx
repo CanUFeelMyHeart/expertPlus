65ed344b3cc986d9f379781716eb268cfa90cc33
julia snegova <ssnnggvv@gmail.com>
2022-03-07 22:00:48 +0300

    physiotherapy page is set

--- medical-clinic-expert/src/pages/ServicesPages/PhysiotherapyServicePage/PhysiotherapyServicePage.jsx
+++ medical-clinic-expert/src/pages/ServicesPages/PhysiotherapyServicePage/PhysiotherapyServicePage.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { InformationContainerComponent } from "../../../components/InformationContainerComponent/InformationContainerComponent";
+import { PriceListComponent } from "../../../components/PriceListComponent/PriceListComponent";
+import { OfertaComponent } from "../../../components/PriceListComponent/OfertaComponent";
+import { priceInfoPhysiotherapy } from "./PriceInfoPhysiotherapy";
 
 export const PhysiotherapyServicePage = () => {
     return (
         <div className="PhysiotherapyServicePage">
-            <h2>Физиотерапия block</h2>
-
+            <InformationContainerComponent header="Физиотерапия">
+                <PriceListComponent priceInfo={priceInfoPhysiotherapy} />
+            </InformationContainerComponent>
+            <OfertaComponent />
         </div>
     );
 };
