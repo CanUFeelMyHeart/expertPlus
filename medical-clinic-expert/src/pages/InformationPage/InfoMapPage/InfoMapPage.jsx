80fbebde3188553d022e5725b56f381866cbd8ac
julia snegova <ssnnggvv@gmail.com>
2022-03-08 00:49:28 +0300

    map page is set

--- medical-clinic-expert/src/pages/InformationPage/InfoMapPage/InfoMapPage.jsx
+++ medical-clinic-expert/src/pages/InformationPage/InfoMapPage/InfoMapPage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { InformationContainerComponent } from "../../../components/InformationContainerComponent/InformationContainerComponent";
+import { InfoMapPageComponent } from "./InfoMapPageComponent";
+import { mapPageInformation } from "./MapPageInformation";
 
 export const InfoMapPage = () => {
     return (
         <div className="InfoMapPage">
-            <InformationContainerComponent header="Карта сайта"></InformationContainerComponent>
+            <InformationContainerComponent header="Карта сайта">
+                <InfoMapPageComponent info={mapPageInformation} />
+            </InformationContainerComponent>
         </div>
     );
 };
