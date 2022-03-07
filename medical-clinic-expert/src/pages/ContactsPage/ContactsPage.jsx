121c9626b8c79e5ba635a86f5b217618a17a0dbc
julia snegova <ssnnggvv@gmail.com>
2022-03-07 23:34:41 +0300

    contacts page is set

--- medical-clinic-expert/src/pages/ContactsPage/ContactsPage.jsx
+++ medical-clinic-expert/src/pages/ContactsPage/ContactsPage.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { InformationContainerComponent } from "../../components/InformationContainerComponent/InformationContainerComponent";
+import { ContactPageInfoComponent } from "./ContactComponent";
+import { contactPageInformation } from "./ContactInfo";
+import "./ContactsPage.css";
 
 export const ContactsPage = () => {
     return (
-        <>
-            <div>contacts page</div>
-        </>
+        <div className="InfoControlOrgPage">
+            <InformationContainerComponent header="Контролирующие организации">
+                {contactPageInformation.map((controlInfo, index) => (
+                    <ContactPageInfoComponent
+                        key={index}
+                        header={controlInfo.header}
+                        info={controlInfo.info}
+                        hr_classname={controlInfo.hr_classname}
+                    />
+                ))}
+            </InformationContainerComponent>
+        </div>
     );
 };
