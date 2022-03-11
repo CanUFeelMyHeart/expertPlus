7098804761ba944872e8260c6acf1b36497449f5
julia snegova <ssnnggvv@gmail.com>
2022-03-11 17:44:42 +0300

    news page is set

--- medical-clinic-expert/src/pages/NewsPage/NewsPage.jsx
+++ medical-clinic-expert/src/pages/NewsPage/NewsPage.jsx
@@ -1,77 +1,48 @@
-// import React, { useState, useMemo } from 'react';
-// import { AboutClinicComponent as AboutClinic } from "../../components/AboutClinicComponent/AboutClinicComponent";
-// import { ServicesComponent } from "../../components/ServicesComponent/ServicesComponent";
-// import { ReviewsComponent } from "../../components/ReviewsComponent/ReviewsComponent";
-// import { InformationComponent } from "../../components/InformationComponent/InformationComponent";
+import React, { useState, useMemo } from "react";
+import { useNavigate } from "react-router-dom";
 import Pagination from "../../components/Pagination/Pagination";
-// export const NewsPage = () => {
+import { InformationContainerComponent } from "../../components/InformationContainerComponent/InformationContainerComponent";
+import data from "./data/news-data.json";
+import "./NewsPage.css";
 
-//     let PageSize = 10;
-//     const [currentPage, setCurrentPage] = useState(1);
-
-//     return (
-//         <>
-//             <div>
-//                 news
-//                 <Pagination
-//                     className="pagination-bar"
-//                     currentPage={currentPage}
-//                     totalCount={10}
-//                     pageSize={PageSize}
-//                     onPageChange={(page) => setCurrentPage(page)}
-//                 />
-//             </div>
-//         </>
-//     );
-// };
-
-import React, { useState, useMemo } from 'react';
-import data from './data/mock-data.json';
-
-let PageSize = 10;
+let PageSize = 6;
 
 export const NewsPage = () => {
   const [currentPage, setCurrentPage] = useState(1);
+  const navigate = useNavigate();
 
-  const currentTableData = useMemo(() => {
+  const currentNewsData = useMemo(() => {
     const firstPageIndex = (currentPage - 1) * PageSize;
     const lastPageIndex = firstPageIndex + PageSize;
     return data.slice(firstPageIndex, lastPageIndex);
   }, [currentPage]);
 
+  function handleClick(ref) {
+    navigate(ref);
+  }
+
   return (
     <>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>FIRST NAME</th>
-            <th>LAST NAME</th>
-            <th>EMAIL</th>
-            <th>PHONE</th>
-          </tr>
-        </thead>
-        <tbody>
-          {currentTableData.map(item => {
-            return (
-              <tr>
-                <td>{item.id}</td>
-                <td>{item.first_name}</td>
-                <td>{item.last_name}</td>
-                <td>{item.email}</td>
-                <td>{item.phone}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      <Pagination
-        className="pagination-bar"
-        currentPage={currentPage}
-        totalCount={data.length}
-        pageSize={PageSize}
-        onPageChange={page => setCurrentPage(page)}
-      />
+      <InformationContainerComponent header="Новости">
+        {currentNewsData.map((singleNew, index) => (
+          <div className="newsPage__new_component">
+            <button
+              onClick={(e) => handleClick(singleNew.ref)}
+              className="newsPage__new_header"
+            >
+              {singleNew.header}
+            </button>
+            <h3 className="newsPage__new_description">{singleNew.description}</h3>
+          </div>
+        ))}
+        <Pagination
+          className="pagination-bar"
+          currentPage={currentPage}
+          totalCount={data.length}
+          pageSize={PageSize}
+          onPageChange={(page) => setCurrentPage(page)}
+        />
+      </InformationContainerComponent>
     </>
   );
-}
+};
