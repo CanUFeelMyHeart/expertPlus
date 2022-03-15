86e87ae9ee1ad1c8cf5bf51d60bdb070c1a3ad55
Mikhail <msmerkushov123@yandex.ru>
2022-03-15 14:21:18 +0300

    fixed bugs

--- medical-clinic-expert/src/pages/InformationPage/InfoOffDocumentsPage/InfoOffDocumentsPage.jsx
+++ medical-clinic-expert/src/pages/InformationPage/InfoOffDocumentsPage/InfoOffDocumentsPage.jsx
@@ -6,7 +6,15 @@ import { InformationContainerComponent } from "../../../components/InformationCo
 export const InfoOffDocumentsPage = () => {
     return (
         <div className="InfoOffDocumentsPage">
-            <InformationContainerComponent header="Официальные документы"></InformationContainerComponent>
+            <InformationContainerComponent header="Официальные документы">
+                <a href="https://drive.google.com/file/d/1L3pXTZcerpMdX204pVcEK1SZYjd0ysy3/view?usp=sharing">Правила внутреннего распорядка для пациентов ООО "Квалифицированная медицинская помощь"</a>
+                <br></br>
+                <a href="https://drive.google.com/file/d/1nMLQEAL5bQEaG3ISUbncV1VXYlwoDhzI/view?usp=sharing">Федеральный закон "Об основах охраны здоровья граждан в РФ"</a>
+                <br></br>
+                <a href="https://drive.google.com/file/d/1J0MZAKJr73x3md1m8KHy6l0HYAghTa0Q/view?usp=sharing">Постановление Правительства РФ от 04.10.2012 N 1006 </a>
+                <br></br>
+            </InformationContainerComponent>
+            
             <AboutClinicComponent />
             <ServicesComponent />
         </div>
