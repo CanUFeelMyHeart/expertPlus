51109bb6c9fe3983a2f69dbef1da4e8171f2e116
julia snegova <ssnnggvv@gmail.com>
2022-03-21 23:36:24 +0300

    test

--- medical-clinic-expert/src/components/ReviewsComponent/CarouselSectionComponent.jsx
+++ medical-clinic-expert/src/components/ReviewsComponent/CarouselSectionComponent.jsx
@@ -2,22 +2,13 @@ import React from "react";
 import "./ReviewsComponent.css";
 
 export const CarouselSectionComponent = (props) => {
-    return (
-        <li id={props.id} tabIndex="0" className="carousel__slide">
-            <div className="carousel__snapper">
-                <a href={props.href_prev} className="carousel__prev">
-                    Go to last slide
-                </a>
-
-                <a href={props.href_next} className="carousel__next">
-                    Go to next slide
-                </a>
-            </div>
-            <div className="carousel__slide_text">
-                <div className="carousel__slide_description">{props.description}</div>
-                <br/>
-                <div className="carousel__slide_name">{props.name}</div>
-            </div>
-        </li>
-    );
+  return (
+    <div className="carousel__slide">
+      <div className="carousel__slide_text">
+        <div className="carousel__slide_description">{props.description}</div>
+        <br />
+        <div className="carousel__slide_name">{props.name}</div>
+      </div>
+    </div>
+  );
 };
--- medical-clinic-expert/src/components/ReviewsComponent/ReviewsComponent.jsx
+++ medical-clinic-expert/src/components/ReviewsComponent/ReviewsComponent.jsx
@@ -1,31 +1,31 @@
 import React from "react";
 import { CarouselSectionComponent } from "./CarouselSectionComponent";
 import { comments } from "./ReviewsInfo";
-import { carousel__navigation } from "./NavInfo";
 import "./ReviewsComponent.css";
+import Carousel from "react-material-ui-carousel";
 
 export const ReviewsComponent = (props) => {
-  const aaaaaa = (e) => {
-    e.preventDefault();
-  };
   return (
     <div className="reviewsComponent__block">
       <h2 className="reviewsComponent__block_header">Отзывы клиентов</h2>
       <div className="reviewsComponent__block_carousel">
-        <section className="carousel" aria-label="Gallery">
-          <ol className="carousel__viewport">
-            {comments.map((comment, index) => (
-              <CarouselSectionComponent
-                key={index}
-                name={comment.name}
-                description={comment.description}
-                id={comment.id}
-                href_prev={comment.href_prev}
-                href_next={comment.href_next}
-              />
-            ))}
-          </ol>
-        </section>
+        <Carousel
+          fullHeightHover={false}
+          swipe={false}
+          indicatorContainerProps={{
+            style: {
+              marginTop: "10px", // 5
+            },
+          }}
+        >
+          {comments.map((comment, index) => (
+            <CarouselSectionComponent
+              key={index}
+              name={comment.name}
+              description={comment.description}
+            />
+          ))}
+        </Carousel>
       </div>
     </div>
   );
