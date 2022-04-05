d129daf606b4ed79e98908dea355429133ef9330
Mikhail <msmerkushov123@yandex.ru>
2022-04-05 18:44:01 +0300

    fixed navigation bar

--- medical-clinic-expert/src/components/SliderComponent/SliderInfo.jsx
+++ medical-clinic-expert/src/components/SliderComponent/SliderInfo.jsx
@@ -1,16 +1,18 @@
-import MainSliderPic1 from "./img/test_carousel.png";
-import MainSliderPic2 from "./img/slide-coronavirus.jpg";
+import MainSliderPic1 from "./img/Frame_134.png";
+import MainSliderPic2 from "./img/Frame_133.png";
+import MainSliderPic3 from "./img/Frame_135.png";
+import MainSliderPic4 from "./img/Frame_136.png";
 export const pictures = [
   {
     url: MainSliderPic1,
   },
   {
-    url: MainSliderPic1,
+    url: MainSliderPic2,
   },
   {
-    url: MainSliderPic1,
+    url: MainSliderPic3,
   },
   {
-    url: MainSliderPic1,
+    url: MainSliderPic4,
   },
 ];
