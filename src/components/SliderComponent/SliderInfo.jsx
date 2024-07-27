673446834e07c78b2f2402f316042d87bd69caab
julia snegova <ssnnggvv@gmail.com>
2024-07-27 15:55:42 +0300

    refactor: edit Slider

--- src/components/SliderComponent/SliderInfo.jsx
+++ src/components/SliderComponent/SliderInfo.jsx
@@ -1,14 +1,6 @@
-import MainSliderPic1 from "./img/Frame_134.png";
-import MainSliderPic2 from "./img/Frame_133.png";
 import MainSliderPic3 from "./img/Frame_135.png";
 import MainSliderPic4 from "./img/Frame_136.png";
 export const pictures = [
-  {
-    url: MainSliderPic1,
-  },
-  {
-    url: MainSliderPic2,
-  },
   {
     url: MainSliderPic3,
   },
