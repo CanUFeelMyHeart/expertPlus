a0bdfaa3023acc46da1a0f89c592246d3bfe8824
julia snegova <ssnnggvv@gmail.com>
2024-07-27 15:52:37 +0300

    add: add licence

--- src/pages/InformationPage/InfoLicencePage/InfoLicencePage.jsx
+++ src/pages/InformationPage/InfoLicencePage/InfoLicencePage.jsx
@@ -1,50 +1,17 @@
 import React from "react";
 import { InformationContainerComponent } from "../../../components/InformationContainerComponent/InformationContainerComponent";
-import pic1 from "../../../assets/images/documents/1.jpg";
-import pic2 from "../../../assets/images/documents/2.jpg";
-import pic3 from "../../../assets/images/documents/3.jpg";
-import pic4 from "../../../assets/images/documents/4.jpg";
-import pic5 from "../../../assets/images/documents/5.jpg";
-import pic6 from "../../../assets/images/documents/6.jpg";
+
+import licence_expert from "assets/documents/licence_expert.pdf";
 import "./InfoLicencePage.css";
 
 export const InfoLicencePage = () => {
-  return (
-    <div className="InfoLicencePage">
-      <InformationContainerComponent header="Лицензии">
-        <div className="InfoLicencePage__container">
-          <div className="InfoLicencePage__picture">
-            <a href={pic1} target="_blank" rel="noreferrer">
-              <img src={pic1} alt="licence1" />
-            </a>
-          </div>
-          <div className="InfoLicencePage__picture">
-            <a href={pic2} target="_blank" rel="noreferrer">
-              <img src={pic2} alt="licence2" />
-            </a>
-          </div>
-          <div className="InfoLicencePage__picture">
-            <a href={pic5} target="_blank" rel="noreferrer">
-              <img src={pic5} alt="licence5" />
-            </a>
-          </div>
-          <div className="InfoLicencePage__picture">
-            <a href={pic6} target="_blank" rel="noreferrer">
-              <img src={pic6} alt="licence6" />
-            </a>
-          </div>
-          <div className="InfoLicencePage__picture">
-            <a href={pic3} target="_blank" rel="noreferrer">
-              <img src={pic3} alt="licence3" />
-            </a>
-          </div>
-          <div className="InfoLicencePage__picture">
-            <a href={pic4} target="_blank" rel="noreferrer">
-              <img src={pic4} alt="licence4" />
-            </a>
-          </div>
-        </div>
-      </InformationContainerComponent>
-    </div>
-  );
+   return (
+      <div className="InfoLicencePage">
+         <InformationContainerComponent header="Лицензии">
+               <a href={licence_expert} target="_blank" rel="noreferrer">
+                  Лицензия Эксперт
+               </a>
+         </InformationContainerComponent>
+      </div>
+   );
 };
